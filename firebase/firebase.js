8f04e10b40cbe87800d181b0a2bea5c10d3042b8
Rahat Mir <91772060+Lifelessdice@users.noreply.github.com>
2025-05-15 11:43:41 +0200

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,10 +1,10 @@
-const admin = require("firebase-admin")
-const serviceAccount = require("./serviceAccountKey.json");
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-
 module.exports = db;
